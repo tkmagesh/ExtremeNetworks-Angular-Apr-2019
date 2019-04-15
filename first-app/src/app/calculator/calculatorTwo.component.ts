edb8f3e8daae45adf2043680f845f84901bd8f01
Magesh <tkmagesh77@gmail.com>
2019-04-15 16:20:30 +0530

    calculator two - solution

---
+++ first-app/src/app/calculator/calculatorTwo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CalculatorModel } from './calculatorModel';
+
+
+@Component({
+	selector : 'app-calculator-2',
+	templateUrl : 'calculatorTwo.component.html'
+})
+export class CalculatorTwoComponent{
+
+	model : CalculatorModel = new CalculatorModel();
+
+}
